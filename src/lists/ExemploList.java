7eb4099d1aefa710e4d56dcbd3dd9b8076fa0c75
Pedro Vaz <pedro0vazs@gmail.com>
2021-09-21 11:44:27 -0300

    Adiciona mais métodos sobre List

--- src/lists/ExemploList.java
+++ src/lists/ExemploList.java
@@ -1,8 +1,6 @@
 package lists;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ExemploList {
 
@@ -31,6 +29,49 @@ public class ExemploList {
 
         // Para vermos se um determinado elemento está dentro de uma lista usamos o método contains(elemento).
         System.out.println("A nota 5.0 está dentro da lista? " + notas.contains(5d));
+
+        // Agora se quisermos buscar um elemento pelo seu indice, temos o método get(indice).
+        System.out.println("A nota que está com o índice 3 é: " + notas.get(3));
+
+        // Para sabermos o menor valor de uma lista ou o maior valor utilizamos o método max(lista) e min(lista)
+        // da classe Collections.
+        System.out.println("A menor nota é: " + Collections.min(notas));
+        System.out.println("A maior nota é: " + Collections.max(notas));
+
+        // Se quisermos somar os elementos de uma lista, vamos precisar do método iterator(), pois as listas não
+        // tem um método nativo para somar os elementos.
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+
+        while(iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+
+        System.out.println("A soma é: " + soma);
+
+        // Para mostrar o tamanho de uma lista usamos o método size().
+        System.out.println("Tamanho da lista: " + notas.size());
+
+        // Para remover algum elemento da lista temos o método remove(), pode-se usar tanto com o indice,
+        // quanto com o próprio elemento.
+        // Quando removemos utilizando como parâmetro o objeto o seu retorno será um boolean, dizendo se o elemento foi
+        // removido(true) ou não(false).
+        var removido = notas.remove(7d);
+        System.out.println("Elemento foi removido? " + removido);
+        System.out.println(notas);
+
+        // Agora se removemos utilizando como parâmetro o índice, seu retorno será o elemento removido.
+        var removido2 = notas.remove(5);
+        System.out.println("Elemento removido: " + removido2);
+        System.out.println(notas);
+
+        // Para apagar todos os elementos da lista, usamos o método clear().
+        notas.clear();
+
+        // Agora se quisermos ver se determinada lista está vazia ou não, utilizamos o método isEmpty().
+        System.out.println(notas.isEmpty());
 
     }
 }
